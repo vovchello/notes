f764806efefa8fd1348bfc1fc9551c4149d35fc2
vovchello <vovka9379@gmail.com>
2018-12-03 01:05:47 +0200

    finishes with shendule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('note:lifecheker')->everyMinute();
+        $schedule->command('note:lifecheker')->daily();
     }
 
     /**
