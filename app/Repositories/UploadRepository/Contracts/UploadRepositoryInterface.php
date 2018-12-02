dca5c9f86ae7e2524018eddda74ad7a903084053
vovchello <vovka9379@gmail.com>
2018-12-02 22:22:15 +0200

    register all apps servises

---
+++ app/Repositories/UploadRepository/Contracts/UploadRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 02.12.18
+ * Time: 22:17
+ */
+
+namespace App\Repositories\UploadRepository\Contracts;
+
+
+class UploadRepositoryInterface
+{
+
+}
---
+++ app/Servises/FileServise/Contact/FileServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 02.12.18
+ * Time: 22:04
+ */
+
+namespace App\Servises\FileServise\Contact;
+
+
+class FileServiceInterface
+{
+
+}
